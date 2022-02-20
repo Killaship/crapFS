6c3027e050c641aa0c3a4988b3d778fcbeeaf891
Killaship <69988679+Killaship@users.noreply.github.com>
2022-02-20 16:45:13 -0500

    Update crapfs.h

--- examples/crapfs.h
+++ examples/crapfs.h
@@ -11,6 +11,7 @@ struct filedata {
 /*
 File permissions are stored in an 8 bit char, in the header of the file. The system this uses takes some inspiration from standard UNIX permissions.
 The first 4 bits are for what the owner or root user can do, and the last 4 bits are for what everyone else can do.
-Each nibble (4 bits) goes rwx
+Each nibble (4 bits) represents the normal rwx, but with a "system" bit at the end, which, if it is active (a one,) only the system can touch that file.
+(System as in kernel, not just the root user.) (Also, this is in terms of OSDev, so we're not talking about the Linux or NT kernel.)
 */
 
