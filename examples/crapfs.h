5474202ba28edc0b6ae3a9e4b873c362c387c2f6
Killaship <69988679+Killaship@users.noreply.github.com>
2022-02-20 22:14:34 -0500

    Update crapfs.h

--- examples/crapfs.h
+++ examples/crapfs.h
@@ -1,8 +1,9 @@
 #define VOL_LABEL "CRAPFS  "
-
+#include <stdbool.h>
 struct filedata {
   char filename[12]; // 8.3 filename including null char
   int null; // if null is marked as one, then this will be treated as the end of the filesystem
+  bool exists;
   long* fileloc; // pointer to file location
   long filesize; // file size, implementation must round to nearest 512 bytes or memes might occur
   char perms; // file permissions, see documentation "mega-comment" lower down
