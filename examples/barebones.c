caafd61dfd27e0e1b0f32f908da6dbbbe9dda372
Killaship <69988679+Killaship@users.noreply.github.com>
2022-02-21 12:00:07 -0500

    Update barebones.c

--- examples/barebones.c
+++ examples/barebones.c
@@ -31,8 +31,8 @@ void listfiles() {
     while((crapfile[i].null) == 0) {
         if((crapfile[i].exists) != 0) {
             printf(crapfile[i].filename);
-            printf(crapfile[i].filesize);
-            printf(crapfile[i].fileloc);
+            //printf(crapfile[i].filesize);
+            //printf(crapfile[i].fileloc);
         }
         else {continue;}
 
