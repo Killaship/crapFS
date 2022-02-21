0acc2396bd80c0e12ef8bc7a6545f32058895860
Killaship <69988679+Killaship@users.noreply.github.com>
2022-02-20 22:23:29 -0500

    Update barebones.c

--- examples/barebones.c
+++ examples/barebones.c
@@ -1,9 +1,9 @@
 #include "crapfs.h" // Include the crapFS structs and data.
 
-struct filedata file[256]; // Initialize the filesystem to 256 files. (Including the blank one marking the end of the filesystem.)
+typedef struct filedata crapfile[256]; // Initialize the filesystem to 256 files. (Including the blank one marking the end of the filesystem.)
 
-file[255].null = 1; // This sets the end of the filesystem at the 256th file.
+crapfile[255].null = 1; // This sets the end of the filesystem at the 256th file.
 
-file.exists = {false}; 
+crapfile.exists = {false}; 
 // This will set all of the "exist" fields in the files to 0, to not mark 
 // as the end of the filesystem, but rather a file that's deleted or absent. 
