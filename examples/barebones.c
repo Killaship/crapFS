0fd0fb7de6e5410f44b45eb7caa44d6f6d51901a
Killaship <69988679+Killaship@users.noreply.github.com>
2022-04-09 16:40:34 -0400

    Update barebones.c

--- examples/barebones.c
+++ examples/barebones.c
@@ -57,7 +57,7 @@ void listfiles() {
 			printf("  ");
 			printf("%p", crapfile[i].fileloc);
 			printf("  ");
-			printf("%lnx", crapfile[entry].fileloc);
+			printf("%x", crapfile[entry].fileloc);
 			printf("  \n");
 			i++;
 		}
