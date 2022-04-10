1c9e4e6bf584b03d65f1db44d049762db51cb4fc
Killaship <69988679+Killaship@users.noreply.github.com>
2022-04-09 21:29:33 -0400

    Update barebones.c

--- examples/barebones.c
+++ examples/barebones.c
@@ -35,7 +35,7 @@ void deletefile() {
 	// TODO: Delete files.
 }
 
-void readfile(char *file, char *outbuf) {
+void readfile(char *file, char **outbuf) {
 	int i = 0;
 	while((crapfile[i].null) == 0) { 
 		if((crapfile[i].exists) != 0) {
@@ -73,7 +73,7 @@ int main(void) {
 	listfiles();
 	printf("\n");
 	char *buff[256];
-	readfile("test.txt", *buff);
+	readfile("test.txt", (char*) buff);
 	printf((char *)buff);
 	return 0;
 }
