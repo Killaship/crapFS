bb59520570f37231cfa69cf37fbeb4e3cc33b6df
Killaship <69988679+Killaship@users.noreply.github.com>
2022-04-10 09:31:31 -0400

    Update barebones.c

--- examples/barebones.c
+++ examples/barebones.c
@@ -40,7 +40,7 @@ void readfile(char *file, char *outbuf) {
 	while((crapfile[i].null) == 0) {  // Loop through FS entries, check if they exist, normal stuff.
 		if((crapfile[i].exists) != 0) { 
 			if(strcmp(crapfile[i].filename, (char *)file) == 0) { // Check if filename of entry is what's requested.
-				strcpy((char *)outbuf, crapfile[i].fileloc); // Copy file contents to output buffer.
+				strcpy((char *)outbuf, (char *)crapfile[i].fileloc); // Copy file contents to output buffer.
 				break; // Close loop to avoid memory memeing. :^)
 			}
 		}
